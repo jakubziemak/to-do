2d6728edc329f81e890839e9a2cbfd405ded89aa
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2021-10-04 15:51:50 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,116 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="container">
+		<h1>TO-DO LIST</h1>
+		<div id="todos">
+			<todos v-for="item in list"
+				v-bind:item="item"
+				@item-to-delete='deleteItem(item)'
+				:key='item.id'/>
+		</div>
+		<div id="footer">
+			<input type="text" placeholder="New task" v-model="task" @keydown.enter="createNewItem()">
+			<img class="add-icon"  v-bind:src=addIcon @click="createNewItem()">
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todos from './components/todos.vue'
+import addIcon from './assets/add_black_24dp.svg'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+export default{
+	name: 'App',
+	components: {
+		todos
+	},
+	data(){
+		return {
+			task: '',
+			list: [],
+			counter: 0,
+			addIcon: addIcon
+		}
+	},
+	methods: {
+		createNewItem: function(){
+			if(this.task.length>0){
+				this.list.push({text: this.task, id: this.counter});
+				this.counter++;
+				this.task = '';
+			}
+		},
+		deleteItem: function(item){
+			const index = this.list.indexOf(item)
+			if (index > -1) {
+				this.list.splice(index, 1);
+			}
+		},
+
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#container{
+	height: 600px;
+	width: 400px;
+	background-color: hsl(212, 11%, 26%);
+	-webkit-box-shadow: 0px 0px 41px -12px rgba(0, 0, 0, 1);
+	-moz-box-shadow: 0px 0px 41px -12px rgba(0, 0, 0, 1);
+	box-shadow: 0px 0px 41px -12px rgba(0, 0, 0, 1);
+
+}
+#todos{
+	height: 70%;
+	position: relative;
+	top: 60px;
+	background-color: hsl(212, 11%, 70%);
+	overflow: auto;
+	display: flex;
+	flex-direction: column;
+}
+#footer{
+	position: relative;
+	bottom: -80px;
+	display: flex;
+	justify-content: space-evenly;
+	width: 90%;
+	left: 50%;
+	transform: translateX(-50%);
+}
+input{
+	width: 260px;
+	border: 0;
+	border-bottom: 2px solid hsl(195, 80%, 40%);
+	color: hsl(195, 80%, 40%);
+	background-color:transparent;
+	font-size: 1rem;
+}
+::placeholder{
+	color: hsl(195, 80%, 40%, .3);
+}
+input:focus{
+	outline: none;
+}
+.add-icon{
+	width: 2rem;
+	cursor: pointer;
+	filter: invert(50%) sepia(43%) saturate(7202%) hue-rotate(169deg) brightness(93%) contrast(84%);
+	transition: filter .2s;
+}
+.add-icon:hover{
+	filter: invert(44%) sepia(22%) saturate(855%) hue-rotate(149deg) brightness(94%) contrast(97%);
+	transition: filter .2s;
+}
+::-webkit-scrollbar{
+	width: 10px;
+	background-color: hsl(0, 0%, 80%);
+}
+::-webkit-scrollbar-thumb{
+	background-color: hsl(0, 0%, 50%);
+}
+::-webkit-scrollbar-thumb:hover{
+	background-color: hsl(0, 0%, 60%);
 }
 </style>
---
+++ src/components/todos.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="task" v-bind:id='item.id'>
+        <p v-bind:class="{active: isActive}" @click="cahangeClass">{{ item.text }}</p>
+        <img class="delete-icon" v-bind:src=deleteIcon @click="deleteItem(item)">
+    </div>
+</template>
+
+<script>
+import deleteIcon from '../assets/delete_black_24dp.svg'
+
+export default{
+    name: 'todos',
+    props: ['item'],
+    data(){
+        return{
+            isActive: true,
+            deleteIcon: deleteIcon
+        }
+    },
+    methods: {
+        cahangeClass: function(){
+            this.isActive = !this.isActive
+        },
+        deleteItem: function(item){
+            this.$emit('item-to-delete', item)
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* #todos{
+    height: 70%;
+    width: 90%;
+    position: relative;
+    top: 60px;
+    border-top: 2px solid hsl(0, 0%, 61%);
+    border-bottom: 2px solid hsl(0, 0%, 61%);
+    left: 50%;
+    transform: translateX(-50%);
+} */
+.task{
+    /* min-height: 2rem; */
+    width: 100%;
+    display: flex;
+    align-items: center;
+    font-size: 1.25rem;
+    margin-top: .4rem;
+    transition: background-color .2s;
+
+}
+.task:hover{
+    background-color: hsla(208, 100%, 97%, .1);
+    transition: background-color .2s;
+}
+.delete-icon{
+    right: 10px;
+    position: absolute;
+    cursor: pointer;
+    width: 1.75rem;
+    transition: filter .2s;
+}
+.delete-icon:hover{
+    transition: filter .2s;
+    filter: invert(39%) sepia(65%) saturate(2887%) hue-rotate(169deg) brightness(93%) contrast(84%);
+}
+p{
+    min-height: 2rem;
+    max-width: 80%;
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+    /* -ms-word-break: break-all; */
+    position: relative;
+    left: 12px;
+    cursor: pointer;
+    text-decoration: line-through;
+    margin: 0;
+    color: hsl(0, 0%, 2%);
+}
+.active{
+    text-decoration: none;
+}
+</style>
